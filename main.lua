088141718f3c04d152fa46ceccc803f0d987c26d
unknown <imogenhergeth@gmail.com>
2020-10-14 14:05:58 +0200

    9.0 changes

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 local ADDON_NAME, _ = ...
 
-local ROLE_ICON_SIZE = 18
-local CLASS_ICON_SIZE = 12
-local ICON_GROUP_SIZE = ROLE_ICON_SIZE + CLASS_ICON_SIZE / 2
+LFGClasses = {
+	isDebug = false,
+	roleDisplay = "icons",
+	ROLE_ICON_SIZE = 18,
+	CLASS_ICON_SIZE = 12,
+}
+LFGClasses.ICON_GROUP_SIZE = LFGClasses.ROLE_ICON_SIZE + LFGClasses.CLASS_ICON_SIZE / 2
+
 
 local ICONS = LFG_LIST_GROUP_DATA_ATLASES
 local ROLES = LFG_LIST_GROUP_DATA_ROLE_ORDER
+
+local eventFrame = CreateFrame("Frame", ADDON_NAME.."EventFrame", UIParent)
 
 local function sort(players)
 	local newPlayers = {}
@@ -65,6 +72,10 @@ local function LFGListGroupDataDisplayRoleClassEnum_Update(self, numPlayers, dis
 end
 
 local function LFGListGroupDataDisplay_UpdateHook(self, activityID, displayData, disabled)
+	if not self.RoleClassEnum then
+		return
+	end
+
 	local fullName, shortName, categoryID, groupID, iLevel, filters, minLevel, maxPlayers, displayType = C_LFGList.GetActivityInfo(activityID)
 	if (displayType == LE_LFG_LIST_DISPLAY_TYPE_ROLE_ENUMERATE) then
 		self.Enumerate:Hide()
@@ -76,46 +87,55 @@ local function LFGListGroupDataDisplay_UpdateHook(self, activityID, displayData,
 end
 hooksecurefunc("LFGListGroupDataDisplay_Update", LFGListGroupDataDisplay_UpdateHook)
 
-for i, button in pairs({LFGListSearchPanelScrollFrameScrollChild:GetChildren()}) do
-	button.DataDisplay.RoleClassEnum =  CreateFrame("Frame", nil, button.DataDisplay)
-	local enum = button.DataDisplay.RoleClassEnum
-	enum:SetAllPoints(button.DataDisplay)
+local function initialize()
+	for i, button in pairs({LFGListSearchPanelScrollFrameScrollChild:GetChildren()}) do
+		button.DataDisplay.RoleClassEnum =  CreateFrame("Frame", nil, button.DataDisplay)
+		local enum = button.DataDisplay.RoleClassEnum
+		enum:SetAllPoints(button.DataDisplay)
 
-	enum.iconGroups = {}
-	for i = 1, 5 do
-		enum.iconGroups[i] = CreateFrame("Frame", nil, enum)
-		local iconGroup = enum.iconGroups[i]
-		iconGroup:SetSize(ICON_GROUP_SIZE, ICON_GROUP_SIZE)
-		if i == 1 then
-			iconGroup:SetPoint("RIGHT", enum, "RIGHT", 0, 0)
-		else
-			iconGroup:SetPoint("CENTER", enum.iconGroups[i - 1], "CENTER", -1 * ICON_GROUP_SIZE, 0)
+		enum.iconGroups = {}
+		for i = 1, 5 do
+			enum.iconGroups[i] = CreateFrame("Frame", nil, enum)
+			local iconGroup = enum.iconGroups[i]
+			iconGroup:SetSize(LFGClasses.ICON_GROUP_SIZE, LFGClasses.ICON_GROUP_SIZE)
+			if i == 1 then
+				iconGroup:SetPoint("RIGHT", enum, "RIGHT", 0, 0)
+			else
+				iconGroup:SetPoint("CENTER", enum.iconGroups[i - 1], "CENTER", -1 * LFGClasses.ICON_GROUP_SIZE, 0)
+			end
+			iconGroup.icons = {}
+
+			iconGroup.role = iconGroup:CreateTexture(nil, "ARTWORK")
+			local roleIcon = iconGroup.role
+			roleIcon:SetSize(LFGClasses.ROLE_ICON_SIZE, LFGClasses.ROLE_ICON_SIZE)
+			roleIcon:SetPoint("TOP", iconGroup, "TOP", 0, 0)
+			roleIcon:SetAtlas("groupfinder-icon-role-large-tank")
+			table.insert(iconGroup.icons, roleIcon)
+
+			iconGroup.class = iconGroup:CreateTexture(nil, "ARTWORK")
+			local classIcon = iconGroup.class
+			classIcon:SetSize(LFGClasses.CLASS_ICON_SIZE, LFGClasses.CLASS_ICON_SIZE)
+			classIcon:SetPoint("BOTTOMRIGHT", iconGroup, "BOTTOMRIGHT", 0, 0)
+			classIcon:SetAtlas("groupfinder-icon-role-large-tank")
+			table.insert(iconGroup.icons, classIcon)
 		end
-		iconGroup.icons = {}
-
-		iconGroup.role = iconGroup:CreateTexture(nil, "ARTWORK")
-		local roleIcon = iconGroup.role
-		roleIcon:SetSize(ROLE_ICON_SIZE, ROLE_ICON_SIZE)
-		roleIcon:SetPoint("TOP", iconGroup, "TOP", 0, 0)
-		roleIcon:SetAtlas("groupfinder-icon-role-large-tank")
-		table.insert(iconGroup.icons, roleIcon)
-
-		iconGroup.class = iconGroup:CreateTexture(nil, "ARTWORK")
-		local classIcon = iconGroup.class
-		classIcon:SetSize(CLASS_ICON_SIZE, CLASS_ICON_SIZE)
-		classIcon:SetPoint("BOTTOMRIGHT", iconGroup, "BOTTOMRIGHT", 0, 0)
-		classIcon:SetAtlas("groupfinder-icon-role-large-tank")
-		table.insert(iconGroup.icons, classIcon)
 	end
 end
 
---[[
+eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:HookScript("OnEvent", function(self, event, ...)
+	if event == "ADDON_LOADED" and ... == ADDON_NAME then
+		initialize()
+		if LFGClasses.isDebug then
+			eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+			eventFrame:SetScript("OnEvent", function(self, ...)
+				PVEFrame_ToggleFrame("GroupFinderFrame")
+				GroupFinderFrameGroupButton4:Click()
+				LFGListCategorySelection_SelectCategory(LFGListFrame.CategorySelection, 2, 0)
+				--dLFGListFrame.CategorySelection.FindGroupButton:Click()
+			end)
+		end
+	end
+end)
 
-local eventFrame = CreateFrame("Frame", ADDON_NAME.."EventFrame", UIParent)
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:SetScript("OnEvent", function(self, ...)
-	PVEFrame_ToggleFrame("GroupFinderFrame")
-	GroupFinderFrameGroupButton4:Click()
-	LFGListCategorySelection_SelectCategory(LFGListFrame.CategorySelection, 2, 0)
-	--dLFGListFrame.CategorySelection.FindGroupButton:Click()
-end)]]
+
